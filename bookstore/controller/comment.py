25ece4d55f2118b993fa89939ca0202abe84d670
QuadnucYard <2380433991@qq.com>
2023-01-24 00:44:50 +0800

    Add post comment

--- bookstore/controller/comment.py
+++ bookstore/controller/comment.py
@@ -1,8 +1,9 @@
 from flask import request
+from flask_login import current_user, login_required
 
 from bookstore.application import app, db
-from bookstore.entity import Comment
-from bookstore.util.result import Result
+from bookstore.entity.comment import Comment
+from bookstore.util.result import Result, model2dict
 
 
 @app.route('/api/book/comments/c<bid>', methods=['GET'])
@@ -13,14 +14,21 @@ def get_comments(bid: str):
 
 
 @app.route('/api/book/comments/post', methods=['POST'])
+@login_required
 def post_comment():
-    bid = request.json.get("bid")  # type:ignore
-    star = request.json.get("star")  # type:ignore
-    content = request.json.get("content")  # type:ignore
-    '''
-    TODO
-    在comment表中插入一条评论
-    
-    '''
+    data = request.json or {}
 
-    return Result.success("成功发布评论")
+    comment = Comment(
+        uid=current_user.id,  # type:ignore
+        uname=current_user.uname,  # type:ignore
+        bid=data["bid"],
+        rating=data["star"],
+        content=data["content"],
+    )
+
+    db.session.add(comment)
+    db.session.commit()  # 不知道为啥，这句执行完后无法从result上获取__dict__内容了
+
+    app.logger.info(comment.id)  # 貌似写了这句就能正常获取__dict__了
+
+    return Result.success("成功发布评论", comment)
